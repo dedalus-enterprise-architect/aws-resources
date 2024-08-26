ab4d8ac88220a03a518ceabff13631f2459bc6ef
Davide Celano <davide.celano@dedalus.com>
2024-08-26 10:28:44 +0200

    add PROFILE var

--- ecr/create_repos_from_file.sh
+++ ecr/create_repos_from_file.sh
@@ -6,20 +6,17 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# AWS Region (update if needed)
-AWS_REGION="eu-west-1"
-
 # Read the file line by line
 while IFS= read -r repo_name; do
   if [ -n "$repo_name" ]; then
     # Create the repository
     echo "Creating repository: $repo_name"
-    aws ecr create-repository --repository-name "$repo_name" --region "$AWS_REGION"
+    aws ecr create-repository --repository-name "$repo_name" --region "$AWS_REGION" --profile "$AWS_PROFILE"
 
     # Check if the repository name does not contain "snapshot"
     if [[ "$repo_name" != *"snapshot"* ]]; then
       echo "Setting image immutability for repository: $repo_name"
-      aws ecr put-image-tag-mutability --repository-name "$repo_name" --image-tag-mutability IMMUTABLE --region "$AWS_REGION"
+      aws ecr put-image-tag-mutability --repository-name "$repo_name" --image-tag-mutability IMMUTABLE --region "$AWS_REGION" --profile "$AWS_PROFILE"
     fi
   fi
 done < "$1"
