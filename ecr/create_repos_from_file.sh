9e5dd14a2f7cd42d84ea7902097167a45fb8f1f9
Davide Celano <davide.celano@dedalus.com>
2024-08-26 10:58:20 +0200

    add parameters for region and profile

--- ecr/create_repos_from_file.sh
+++ ecr/create_repos_from_file.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# Check if the input file is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <repositories_file>"
+# Check if the required parameters are provided
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <repositories_file> <aws_region> <aws_profile>"
   exit 1
 fi
+
+# Parameters
+REPOSITORIES_FILE="$1"
+AWS_REGION="$2"
+AWS_PROFILE="$3"
+
 
 # Read the file line by line
 while IFS= read -r repo_name; do
   if [ -n "$repo_name" ]; then
     # Create the repository
     echo "Creating repository: $repo_name"
-    aws ecr create-repository --repository-name "$repo_name" --region "$AWS_REGION" --profile "$AWS_PROFILE"
+    aws ecr create-repository --repository-name "$repo_name" --region $AWS_REGION --profile $AWS_PROFILE
 
     # Check if the repository name does not contain "snapshot"
     if [[ "$repo_name" != *"snapshot"* ]]; then
       echo "Setting image immutability for repository: $repo_name"
-      aws ecr put-image-tag-mutability --repository-name "$repo_name" --image-tag-mutability IMMUTABLE --region "$AWS_REGION" --profile "$AWS_PROFILE"
+      aws ecr put-image-tag-mutability --repository-name "$repo_name" --image-tag-mutability IMMUTABLE --region $AWS_REGION --profile $AWS_PROFILE
     fi
   fi
-done < "$1"
+done < "$REPOSITORIES_FILE"
 
 echo "All repositories have been processed."
